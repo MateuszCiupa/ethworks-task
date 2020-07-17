8b8da758e53775476b45a60457dee3f3973f3521
MateuszCiupa <01131212@pw.edu.pl>
2020-07-17 18:29:38 +0200

    example of usage

--- index.js
+++ index.js
@@ -5,6 +5,20 @@ module.exports = {
     getSumOfExpressions
 };
 
+// Example of usage
+
+var expression1 = Expression();
+expression1.insert(1, 3);
+expression1.insert(2, 5);
+
+var expression2 = Expression();
+expression2.insert(3, 1);
+expression2.insert(3, 3);
+
+var result = getSumOfExpressions(expression1, expression2);
+
+console.log(String(result));
+
 // **********
 
 function Term(constant = 0, exponent = 0) {
